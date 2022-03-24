be2af6bb392e3798b1ceb3e3ed86594d80beca33
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2022-03-24 19:23:32 +0900

    _buttonRadius OK

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,6 +8,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  double _buttonRadius = 100;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,16 +36,18 @@ class _HomePageState extends State<HomePage> {
   // 円形のコンテナの関数
   Widget _circularAnimationButton() {
     // GestureDetectorは、画面のタップやスワイプに関する処理を実装することができるので、アプリ開発には欠かせないウィジェット
-    return GestureDetector(
-      onTap: () {
-        print("Tapped");
-      },
-      child: Container(
-        height: 100,
-        width: 100,
-        decoration: BoxDecoration(
-          color: Colors.purple,
-          borderRadius: BorderRadius.circular(100),
+    return Center( // Centarでラップするとボタンを中央寄せにする
+      child: GestureDetector(
+        onTap: () {
+        },
+        child: AnimatedContainer(
+          duration: const Duration(seconds: 2), // 持続時間2秒
+          height: 100,
+          width: _buttonRadius,
+          decoration: BoxDecoration(
+            color: Colors.purple,
+            borderRadius: BorderRadius.circular(_buttonRadius),
+          ),
         ),
       ),
     );
