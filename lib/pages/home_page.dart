a063f4dc8a7403bb7e90174ff3d3a7fc8870e0bf
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2022-03-24 19:14:37 +0900

    circle OK

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -14,7 +14,10 @@ class _HomePageState extends State<HomePage> {
       body: Container(
         child: Stack(
           clipBehavior: Clip.none,
-          children: [_pageBackground()],
+          children: [
+            _pageBackground(),
+            _circularAnimationButton(),
+            ],
         ),
       ),
     );
@@ -24,6 +27,15 @@ class _HomePageState extends State<HomePage> {
   Widget _pageBackground() {
     return Container(
       color: Colors.blue,
+    );
+  }
+
+  // 円形のコンテナの関数
+  Widget _circularAnimationButton() {
+    return Container(
+      height: 100,
+      width: 100,
+      decoration: BoxDecoration(color: Colors.purple, borderRadius: BorderRadius.circular(100)),
     );
   }
 }
